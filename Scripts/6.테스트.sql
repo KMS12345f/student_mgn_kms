9a9468e7bfeefec0c39f07c053662d49392a70e3
kms <ddllddll3@naver.com>
2021-08-31 17:59:43 +0900

    오후 DB자료

---
+++ Scripts/6.테스트.sql
@@ -0,0 +1,51 @@
+-- 학생이 수강한 과목
+
+select s.std_no, s.std_name, j.subj_name , e.score 
+  from student s join enroll e 
+    on s.std_no = e.std_no
+  join subject j 
+    on e.subj_code = j.subj_code;
+    
+-- 
+번호  성명  컴퓨터개론 프리토킹 리스닝 데이터과학 빅데이터 합계  평균
+101 안성용    96     88    79   null   null 263  87.6
+102 최명기   null   null  null   66     56  122  61
+103 김태수   null    67   null   86    null 153  76.5
+;
+-- pivot 사용하여 위와 같이 출력
+create view vw_student_grade as
+select std_no '학번'
+, std_name '성명'
+, max(if(subj_code = 501, score, null)) as '컴퓨터 개론'
+, max(if(subj_code = 502, score, null)) as '프리토킹'
+, max(if(subj_code = 503, score, null)) as '리스닝'
+, max(if(subj_code = 504, score, null)) as '데이터 과학'
+, max(if(subj_code = 505, score, null)) as '빅데이터'
+, sum(score) '합계'
+, avg(score) '평균'
+  from(select s.std_no, s.std_name, j.subj_name, j.subj_code, e.score 
+  from student s join enroll e 
+    on s.std_no = e.std_no
+  join subject j 
+    on e.subj_code = j.subj_code) t
+  group by std_no;
+ 
+select 학번, 성명, `컴퓨터 개론`, 프리토킹, 리스닝, `데이터 과학`, 빅데이터, 합계, 평균
+  from vw_student_grade;
+
+drop view vw_student_grade;
+
+
+-- 101 학생이 수강한 과목을 알고자 할 경우
+select s.std_no, s.std_name, e.subj_code, j.subj_name
+  from student s join enroll e
+    on s.std_no = e.std_no 
+  join subject j
+    on e.subj_code = j.subj_code
+ where s.std_no = 101;
+
+-- '프리토킹' 과목을 수강한 학생
+select s.subj_code, s.subj_name, std.std_no, std.std_name, std.std_dept, std.std_tel 
+  from subject s join enroll e on s.subj_code = e.subj_code
+  join student std on std.std_no = e.std_no
+ where s.subj_code = 502;
